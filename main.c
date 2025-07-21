047e5f6dfdfd379d5c168823c4abfa50ea9b611f
annes <anneswidow@gmail.com>
2025-07-21 20:29:56 +0300

    Implement folder creation and error handling; add configuration for IntelliSense

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include "raylib.h"
 #include "ui.h"
-
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdio.h>
+//#include <unistd.h>
+int createFolder(const char *folderName) {
+    int status = mkdir(folderName, 0755);
+    if (status == 0) {
+        printf("Folder created: %s\n", folderName);
+        return 0;
+    } else {
+        perror("mkdir failed");
+        return -1;
+    }
+}
+int createFileWithText(const char *filePath, const char *text) {
+    FILE *fp = fopen(filePath, "w");
+    if (!fp) {
+        perror("File open failed");
+        return -1;
+    }
+    fprintf(fp, "%s", text);
+    fclose(fp);
+    printf("File created: %s\n", filePath);
+    return 0;
+}
 int main() {
     InitWindow(800, 600, "Button Test");
 
@@ -8,8 +33,11 @@ int main() {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        if (uiButton((Rectangle){100, 100, 150, 40}, "Hello")) {
-            TraceLog(LOG_INFO, "Clicked!");
+        if (uiButton((Rectangle){100, 100, 150, 40}, "Make new project")) {
+            TraceLog(LOG_INFO, "NewProject");
+            if (createFolder("Project") != 0) {
+                DrawText("An error occured.", 0,0, 20, BLACK);
+            }
         }
 
         EndDrawing();
